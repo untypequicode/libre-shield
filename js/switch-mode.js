ff681e4d52c0ad93e3548e2ed9381df9998bb674
untypequicode <untypequicode@gmail.com>
2024-01-07 00:07:10 +0100

    update transition mode

--- js/switch-mode.js
+++ js/switch-mode.js
@@ -1,11 +1,26 @@
 var switchmode = document.getElementById("switch-mode");
 
-switchmode.onclick = function() {
-    document.body.classList.toggle("dark-mode");
-    if(document.body.classList.contains("dark-mode")) {
+// Fonction pour détecter le mode de thème préféré de l'utilisateur
+function detectPreferredTheme() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add("dark-mode");
         switchmode.src = "images/moon.svg";
-    }
-    else {
+    } else {
+        document.body.classList.remove("dark-mode");
         switchmode.src = "images/sun.svg";
     }
 }
+
+// Appel initial pour détecter le thème au chargement de la page
+detectPreferredTheme();
+
+// Gestionnaire de clic pour basculer entre les modes clair et sombre
+switchmode.onclick = function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        switchmode.src = "images/moon.svg";
+    } else {
+        switchmode.src = "images/sun.svg";
+    }
+};
