a8bece983c368d90fa4a933cc4ba50765d9fab7a
untypequicode <untypequicode@gmail.com>
2024-02-04 21:59:06 +0100

    update base-donnees-cinema-series.js & base-donnees-applis.js - update documentation

--- js/base-donnees-cinema-series.js
+++ js/base-donnees-cinema-series.js
@@ -159,10 +159,24 @@ document.addEventListener('DOMContentLoaded', () => {
 /**
  * Génère le HTML pour un élément de contenu avec des classes spécifiques.
  *
+ * Cette fonction construit un élément de contenu HTML en utilisant des informations
+ * fournies dans l'objet `contenu`, tout en permettant une personnalisation poussée
+ * grâce à des classes CSS dynamiques. Les classes sont déterminées par les paramètres
+ * de la fonction, permettant une flexibilité dans la mise en forme et l'affichage.
+ *
  * @param {Object} contenu - L'objet contenant les informations de l'élément à afficher.
- * @param {String} prefixeClasse - Préfixe à ajouter aux classes de l'élément pour une personnalisation CSS.
- * @param {String} classeAlternee - Détermine la position de l'image ('left' ou 'right') pour alterner l'affichage.
- * @return {String} Le HTML généré pour l'élément de contenu.
+ *     Les propriétés attendues incluent `classe`, `type`, `id`, `imageSrc`, `imageAlt`,
+ *     `titre`, `description`, `liens`, etc.
+ * @param {String} prefixeClasse - Préfixe à ajouter aux classes de l'élément pour une
+ *     personnalisation CSS. Ce préfixe est appliqué devant chaque classe spécifique à l'élément.
+ * @param {String} styleClasse - Classe supplémentaire pour appliquer un style spécifique
+ *     à l'ensemble de l'élément. Peut être utilisé pour modifier l'apparence globale de
+ *     l'élément de contenu, tel que sa couleur de fond, marges, etc.
+ * @param {String} classeAlternee - Détermine la position de l'image ('left' ou 'right') pour
+ *     alterner l'affichage des images dans une liste d'éléments. Permet de créer un effet
+ *     visuel alterné dans la mise en page.
+ * @return {String} Le HTML généré pour l'élément de contenu. Ce HTML peut être inséré
+ *     directement dans le DOM via des méthodes telles que `innerHTML` ou `insertAdjacentHTML`.
  */
 function genererTexte(contenu, prefixeClasse, styleClasse, classeAlternee) {
     // Début de la construction du HTML avec les classes dynamiques et l'ID de l'élément
