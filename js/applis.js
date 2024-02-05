6be131de54f8baa845f954c5bdd83f893eef284f
untypequicode <untypequicode@gmail.com>
2024-02-05 13:43:53 +0100

    update with new js system

---
+++ js/applis.js
@@ -0,0 +1,93 @@
+const contenus = [
+    {
+        classe: "app",
+        id: "whatsapp",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/whatsapp.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Whatsapp",
+        type: "origine",
+        date: "2009-02-00",
+        description: "Simple, fiable, privé, messagerie et appels gratuits, disponible partout dans le monde.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://www.whatsapp.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "signal" ]
+    },
+    {
+        classe: "app",
+        id: "signal",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/signal.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Signal",
+        type: "alternative",
+        date: "2018-01-10",
+        description: "Dites « Bonjour » à une expérience de messagerie différente. " +
+            "Un accent inattendu sur la confidentialité et la protection des données personnelles, " +
+            "combiné à toutes les fonctions auxquelles vous vous attendez.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://www.signal.org/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "whatsapp" ]
+    },
+    {
+        classe: "app",
+        id: "gdrive",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/google-drive.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Google Drive",
+        type: "origine",
+        date: "2012-05-24",
+        description: "Stockez vos fichiers et dossiers, partagez-les avec vos collaborateurs, " +
+            "et travaillez dessus depuis votre appareil mobile, tablette ou ordinateur",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://www.whatsapp.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "mega", "kdrive" ]
+    },
+    {
+        classe: "app",
+        id: "mega",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/mega.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "MEGA",
+        type: "alternative",
+        date: "2013-01-19",
+        description: "La protection des données personnelles et la confidentialité " +
+            "ne sont pas des options avec MEGA, c’est la norme. " +
+            "Nous croyons en effet que tout le monde devrait pouvoir stocker des données et " +
+            "communiquer en ligne en toute sécurité et confidentialité.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://www.whatsapp.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "gdrive" ]
+    },
+    {
+        classe: "app",
+        id: "kdrive",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/kdrive.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "KDrive",
+        type: "alternative",
+        date: "2009-02-00",
+        description: "Stockez, partagez et synchronisez vos données dans un cloud éthique.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://www.whatsapp.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "gdrive" ]
+    }
+];
