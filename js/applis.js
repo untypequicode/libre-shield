a49b3ec455b9b168883ad434fcb96b4753e97470
untypequicode <untypequicode@gmail.com>
2024-02-21 12:28:30 +0100

    update icon brave & add other icons

--- js/applis.js
+++ js/applis.js
@@ -110,7 +110,8 @@ const contenus = [
         liens: [
             { classe: "app-link", href: "https://www.google.com/intl/fr/chrome/", target: "_blank", texte: "Site officiel" }
         ],
-        apps: [ "brave", "firefox" ]
+        apps: [ "brave", "firefox" ],
+        utilisateurs: 3000000000 // Nombre d'utilisateurs en millions
     },
     {
         classe: "app",
@@ -127,7 +128,8 @@ const contenus = [
         liens: [
             { classe: "app-link", href: "https://www.mozilla.org/fr/firefox/", target: "_blank", texte: "Site officiel" }
         ],
-        apps: [ "chrome" ]
+        apps: [ "chrome" ],
+        utilisateurs: 250000000 // Nombre d'utilisateurs en millions
     },
     {
         classe: "app",
@@ -144,6 +146,139 @@ const contenus = [
         liens: [
             { classe: "app-link", href: "https://brave.com/", target: "_blank", texte: "Site officiel" }
         ],
-        apps: [ "chrome" ]
+        apps: [ "chrome" ],
+        utilisateurs: 25000000 // Nombre d'utilisateurs
+    },
+    {
+        classe: "app",
+        id: "gmail",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/gmail.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Gmail",
+        type: "origine",
+        date: "2004-04-01",
+        description: "Gmail est un service de messagerie électronique intuitif, efficace et utile. " +
+            "Il est accessible sur n'importe quel appareil.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://mail.google.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "protonmail", "infomaniak", "tutanota" ],
+        utilisateurs: 1500000000 // Nombre d'utilisateurs
+    },
+    {
+        classe: "app",
+        id: "protonmail",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/proton-mail.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "ProtonMail",
+        type: "alternative",
+        date: "2014-05-16",
+        description: "ProtonMail est le plus grand service de messagerie sécurisée au monde, " +
+            "développé par des scientifiques du CERN et du MIT.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://protonmail.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "gmail" ],
+        utilisateurs: 5000000 // Nombre d'utilisateurs
+    },
+    {
+        classe: "app",
+        id: "infomaniak",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/infomaniak.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Infomaniak",
+        type: "alternative",
+        date: "1994-11-00",
+        description: "Infomaniak est le leader suisse de l'hébergement web, " +
+            "enregistrement de noms de domaine et solutions cloud.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://www.infomaniak.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "gmail" ],
+        utilisateurs: 500000 // Nombre d'utilisateurs
+    },
+    {
+        classe: "app",
+        id: "tutanota",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/tutanota.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Tutanota",
+        type: "alternative",
+        date: "2011-07-01",
+        description: "Tutanota est le service de messagerie électronique sécurisé, " +
+            "qui vous permet d'envoyer des e-mails chiffrés à n'importe qui.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://tutanota.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "gmail" ],
+        utilisateurs: 5000000 // Nombre d'utilisateurs
+    },
+    {
+        classe: "app",
+        id: "windows",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/windows.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Windows",
+        type: "origine",
+        date: "1985-11-20",
+        description: "Windows 10 offre des fonctionnalités de pointe pour vous aider à être plus créatif et productif.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://www.microsoft.com/fr-fr/windows", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "popos", "ubuntu"],
+        utilisateurs: 1000000000 // Nombre d'utilisateurs
+    },
+    {
+        classe: "app",
+        id: "popos",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/pop-os.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Pop!_OS",
+        type: "alternative",
+        date: "2017-10-19",
+        description: "Pop!_OS est un système d'exploitation basé sur Ubuntu, conçu pour la créativité " +
+            "et la productivité, avec une interface utilisateur élégante et des fonctionnalités avancées.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://pop.system76.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "windows" ],
+        utilisateurs: 500000 // Nombre d'utilisateurs
+    },
+    {
+        classe: "app",
+        id: "ubuntu",
+        contenuClasse: "app-content",
+        imageSrc: "images/apps/ubuntu.svg",
+        imageAlt: "illustration",
+        texteClasse: "app-text",
+        titre: "Ubuntu",
+        type: "alternative",
+        date: "2004-10-20",
+        description: "Ubuntu est un système d'exploitation open source, basé sur Linux, " +
+            "pour les ordinateurs personnels, les serveurs et les appareils IoT.",
+        liensClasse: "app-links",
+        liens: [
+            { classe: "app-link", href: "https://ubuntu.com/", target: "_blank", texte: "Site officiel" }
+        ],
+        apps: [ "windows" ],
+        utilisateurs: 40000000 // Nombre d'utilisateurs
     }
 ];
