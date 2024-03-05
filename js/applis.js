2c0003ee369dce8e2dc34fd3acd632c51f19c6c9
untypequicode <untypequicode@gmail.com>
2024-03-06 00:04:33 +0100

    update uix applis applis.html & applis.css & applis.js

--- js/applis.js
+++ js/applis.js
@@ -1,4 +1,4 @@
-const category = [
+const categories = [
     {
         id: "navigateur",
         name: "Navigateur",
@@ -8,6 +8,31 @@ const category = [
         id: "messagerie-instantanee",
         name: "Messagerie instantanée",
         apps: [ "whatsapp", "telegram", "signal" ]
+    },
+    {
+        id: "stockage-fichiers",
+        name: "Stockage de fichiers",
+        apps: [ "gdrive", "mega", "kdrive" ]
+    },
+    {
+        id: "messagerie-electronique",
+        name: "Messagerie électronique",
+        apps: [ "gmail", "protonmail", "infomaniak", "tutanota" ]
+    },
+    {
+        id: "systeme-exploitation",
+        name: "Système d'exploitation",
+        apps: [ "windows", "popos", "ubuntu" ]
+    },
+    {
+        id: "retouche-images",
+        name: "Retouche d'images",
+        apps: [ "photoshop", "gimp" ]
+    },
+    {
+        id: "editeur-code",
+        name: "Éditeur de code source",
+        apps: [ "vscode", "vscodium" ]
     }
 ]
 
@@ -79,6 +104,146 @@ const apps = [
         description: "Application de messagerie instantanée.",
         liens: [
             { name: "Site officiel", url: "https://signal.org/" }
+        ],
+        notation: 5
+    },
+    {
+        id: "gdrive",
+        name: "Google Drive",
+        icon: "images/apps/google-drive.svg",
+        description: "Service de stockage et de partage de fichiers.",
+        liens: [
+            { name: "Site officiel", url: "https://www.google.com/drive/" }
+        ],
+        notation: 3
+    },
+    {
+        id: "mega",
+        name: "Mega",
+        icon: "images/apps/mega.svg",
+        description: "Service de stockage et de partage de fichiers.",
+        liens: [
+            { name: "Site officiel", url: "https://mega.nz/" }
+        ],
+        notation: 4
+    },
+    {
+        id: "kdrive",
+        name: "KDrive",
+        icon: "images/apps/kdrive.svg",
+        description: "Service de stockage et de partage de fichiers.",
+        liens: [
+            { name: "Site officiel", url: "https://www.infomaniak.com/fr/kdrive" }
+        ],
+        notation: 5
+    },
+    {
+        id: "gmail",
+        name: "Gmail",
+        icon: "images/apps/gmail.svg",
+        description: "Service de messagerie électronique.",
+        liens: [
+            { name: "Site officiel", url: "https://mail.google.com/" }
+        ],
+        notation: 3
+    },
+    {
+        id: "protonmail",
+        name: "ProtonMail",
+        icon: "images/apps/proton-mail.svg",
+        description: "Service de messagerie électronique.",
+        liens: [
+            { name: "Site officiel", url: "https://protonmail.com/" }
+        ],
+        notation: 5
+    },
+    {
+        id: "infomaniak",
+        name: "Infomaniak",
+        icon: "images/apps/infomaniak.svg",
+        description: "Service de messagerie électronique.",
+        liens: [
+            { name: "Site officiel", url: "https://www.infomaniak.com/fr/mail" }
+        ],
+        notation: 4
+    },
+    {
+        id: "tutanota",
+        name: "Tutanota",
+        icon: "images/apps/tutanota.svg",
+        description: "Service de messagerie électronique.",
+        liens: [
+            { name: "Site officiel", url: "https://tutanota.com/" }
+        ],
+        notation: 5
+    },
+    {
+        id: "windows",
+        name: "Windows",
+        icon: "images/apps/windows.svg",
+        description: "Système d'exploitation.",
+        liens: [
+            { name: "Site officiel", url: "https://www.microsoft.com/fr-fr/windows" }
+        ],
+        notation: 1
+    },
+    {
+        id: "popos",
+        name: "Pop!_OS",
+        icon: "images/apps/pop-os.svg",
+        description: "Système d'exploitation.",
+        liens: [
+            { name: "Site officiel", url: "https://pop.system76.com/" }
+        ],
+        notation: 5
+    },
+    {
+        id: "ubuntu",
+        name: "Ubuntu",
+        icon: "images/apps/ubuntu.svg",
+        description: "Système d'exploitation.",
+        liens: [
+            { name: "Site officiel", url: "https://ubuntu.com/" }
+        ],
+        notation: 5
+    },
+    {
+        id: "photoshop",
+        name: "Adobe Photoshop",
+        icon: "images/apps/photoshop.svg",
+        description: "Logiciel de retouche d'images.",
+        liens: [
+            { name: "Site officiel", url: "https://www.adobe.com/fr/products/photoshop.html" }
+        ],
+        notation: 1
+    },
+    {
+        id: "gimp",
+        name: "GIMP",
+        icon: "images/apps/gimp.svg",
+        description: "Logiciel de retouche d'images.",
+        liens: [
+            { name: "Site officiel", url: "https://www.gimp.org/" }
+        ],
+        notation: 5
+    },
+    {
+        id: "vscode",
+        name: "Visual Studio Code",
+        icon: "images/apps/vscode.svg",
+        description: "Éditeur de code source.",
+        liens: [
+            { name: "Site officiel", url: "https://code.visualstudio.com/" }
+        ],
+        notation: 5
+    },
+    {
+        id: "vscodium",
+        name: "VSCodium",
+        icon: "images/apps/vscodium.svg",
+        description: "Éditeur de code source.",
+        liens: [
+            { name: "Site officiel", url: "https://vscodium.com/" }
         ],
         notation: 5
     }
--- js/base-donnees-applis.js
+++ js/base-donnees-applis.js
@@ -1,7 +1,77 @@
 function genereContent() {
     document.querySelectorAll('.category').forEach(element => element.remove());
 
-    
+    let i = 0;
+    // Calculer le nombre d'applications pour chaque catégorie
+    const nbAppsByCategory = categories.map(category => {
+        return {
+            ...category,
+            nbApps: category.apps.length
+        };
+    });
+
+    // Trier les catégories par nombre d'applications décroissant
+    nbAppsByCategory.sort((a, b) => {
+        return b.nbApps - a.nbApps;
+    });
+
+    // Générer le contenu HTML pour chaque catégorie triée
+    nbAppsByCategory.forEach(category => {
+        const second = i % 2 === 0 ? '' : 'second';
+        let content = `
+        <section class="category ${second}">
+            <div class="category-title" id="${category.id}">
+                <h2>${category.name}</h2>
+            </div>
+
+            <div class="category-content">
+        `;
+
+        let maxNotation = 0;
+        category.apps.forEach(app => {
+            const appData = apps.find(appData => appData.id === app);
+            if (appData.notation > maxNotation) {
+                maxNotation = appData.notation;
+            }
+        });
+
+        // Trier les applications par notation décroissante
+        category.apps.sort((a, b) => {
+            const appAData = apps.find(appData => appData.id === a);
+            const appBData = apps.find(appData => appData.id === b);
+
+            return appBData.notation - appAData.notation;
+        });
+
+        category.apps.forEach(app => {
+            const appData = apps.find(appData => appData.id === app);
+
+            let color = '';
+            if (appData.notation / maxNotation > .8) {
+                color = 'green';
+            } else if (appData.notation / maxNotation > .6) {
+                color = 'yellow';
+            } else if (appData.notation / maxNotation > .4) {
+                color = 'orange';
+            } else if (appData.notation / maxNotation > .2) {
+                color = 'red';
+            } else {
+                color = 'purple';
+            }
+
+            content += `
+            <div class="app  ${color}">
+                <img src="${appData.icon}" alt="${appData.name}">
+                <h3>${appData.name}</h3>
+            </div>
+            `;
+        });
+
+        content += `</div>
+        </section>`;
+        document.body.insertAdjacentHTML('beforeend', content);
+        i++;
+    });
 }
 
 // Attente que le document soit entièrement chargé pour assurer que tous les éléments sont accessibles.
