496bcc0c28d84665875a5ecb24757101c24aac19
untypequicode <untypequicode@gmail.com>
2024-05-27 01:50:09 +0200

    Update of the CSS of the cinema page for better project maintenance. The JavaScript part of this page still needs to be rewritten.

--- js/base-donnees.js
+++ js/base-donnees.js
@@ -11,38 +11,30 @@
  *     `titre`, `description`, `liens`, etc.
  * @param {String} prefixeClasse - Préfixe à ajouter aux classes de l'élément pour une
  *     personnalisation CSS. Ce préfixe est appliqué devant chaque classe spécifique à l'élément.
- * @param {String} styleClasse - Classe supplémentaire pour appliquer un style spécifique
+ * @param {String} styleSection - Classe supplémentaire pour appliquer un style spécifique
  *     à l'ensemble de l'élément. Peut être utilisé pour modifier l'apparence globale de
  *     l'élément de contenu, tel que sa couleur de fond, marges, etc.
- * @param {String} classeAlternee - Détermine la position de l'image ('left' ou 'right') pour
- *     alterner l'affichage des images dans une liste d'éléments. Permet de créer un effet
- *     visuel alterné dans la mise en page.
+ * @param {String} styleBtn - Classe supplémentaire pour appliquer un style spécifique aux
+ *     boutons de lien. Peut être utilisé pour modifier l'apparence des boutons de lien
+ *     dans l'élément de contenu, tels que la couleur du texte, la taille, etc.
  * @return {String} Le HTML généré pour l'élément de contenu. Ce HTML peut être inséré
  *     directement dans le DOM via des méthodes telles que `innerHTML` ou `insertAdjacentHTML`.
  */
-function genererTexte(contenu, prefixeClasse, styleClasse, classeAlternee) {
+function genererTexte(contenu, prefixeClasse, styleSection, styleBtn) {
     // Début de la construction du HTML avec les classes dynamiques et l'ID de l'élément
-    let resume = `<section class="${prefixeClasse}${contenu.classe} ${styleClasse} ${contenu.type}" id="${contenu.id}"><div class="${prefixeClasse}${contenu.contenuClasse}">`;
+    let resume = `<section class="${prefixeClasse}${contenu.classe} ${styleSection} ${contenu.type}" id="${contenu.id}"><div class="${prefixeClasse}${contenu.contenuClasse}">`;
 
-    // Condition pour ajouter l'image à gauche si classeAlternee est 'left'
-    if(classeAlternee === 'left') {
-        resume += `<img src="${contenu.imageSrc}" alt="${contenu.imageAlt}">`;
-    }
+        resume += `<img src="${contenu.imageSrc}" alt="${contenu.imageAlt}" class="section-img">`;
 
     // Ajout du titre, de la description et des liens
-    resume += `<div class="${prefixeClasse}${contenu.texteClasse} ${styleClasse}">`;
+    resume += `<div class="${prefixeClasse}${contenu.texteClasse}">`;
     resume += `<h1>${contenu.titre}</h1>`;
     resume += `<p>${contenu.description}</p>`;
-    resume += `<div class="${prefixeClasse}${contenu.liensClasse} ${styleClasse}">`;
+    resume += `<div class="${prefixeClasse}${contenu.liensClasse}">`;
     contenu.liens.forEach(lien => {
-        resume += `<a class="btn-c ${prefixeClasse}${lien.classe} ${styleClasse}" href="${lien.href}" target="${lien.target}">${lien.texte}</a>`;
+        resume += `<a class="btn-c ${prefixeClasse}${lien.classe} ${styleBtn}" href="${lien.href}" target="${lien.target}">${lien.texte}</a>`;
     });
     resume += `</div></div>`; // Fermeture des divs de texte et de liens
-
-    // Condition pour ajouter l'image à droite si classeAlternee est 'right'
-    if(classeAlternee === 'right') {
-        resume += `<img src="${contenu.imageSrc}" alt="${contenu.imageAlt}">`;
-    }
 
     // Fermeture des balises de contenu et de section, puis retour du HTML généré
     resume += `</div></section>`;
---
+++ js/db-cinema-series.js
@@ -0,0 +1,120 @@
+const db_cinema_series = [
+    {
+        classe: "cine",
+        id: "dnedf",
+        contenuClasse: "sh-v section-content",
+        imageSrc: "images/cinema-series/films/derriere-nos-ecrans-de-fumees-horizontal-0.jpg",
+        imageAlt: "illustration",
+        texteClasse: "sv section-text",
+        titre: "Derriere nos écrans de fumées",
+        type: "film",
+        date: "2020-09-09",
+        description: "À l'heure où les plateformes numériques deviennent essentielles pour rester en contact," +
+            "les experts révèlent comment elles sont en train de reprogrammer notre civilisation en montrant ce qui se cache derrière nos écrans.",
+        liensClasse: "cine-links",
+        liens: [
+            { classe: "cine-link", href: "https://www.netflix.com/fr/title/81254224", target: "_blank", texte: "Voir sur Netflix" },
+            { classe: "cine-link", href: "https://www.imdb.com/title/tt11464826/", target: "_blank", texte: "Voir sur IMDB" }
+        ]
+    },
+    {
+        classe: "cine",
+        id: "s",
+        contenuClasse: "sh-v section-content",
+        imageSrc: "images/cinema-series/series/stalk-horizontal-0.jpg",
+        imageAlt: "illustration",
+        texteClasse: "sv section-text",
+        titre: "Stalk",
+        type: "serie",
+        date: "2020-03-13",
+        description: "Quand il entre dans une école d'ingénieurs de haut niveau, Lucas alias Lux," +
+            "un geek surdoué de 18 ans, est humilié par les autres étudiants." +
+            "Il décide de se venger à l'aide de sa plus grande arme: son don pour le cyber-harcèlement.",
+        liensClasse: "cine-links",
+        liens: [
+            {
+                classe: "cine-link",
+                href: "https://www.france.tv/slash/stalk/",
+                target: "_blank",
+                texte: "Voir sur France.tv"
+            },
+            {
+                classe: "cine-link",
+                href: "https://www.imdb.com/title/tt10133370/",
+                target: "_blank",
+                texte: "Voir sur IMDB"
+            }
+        ]
+    },
+    {
+        classe: "cine",
+        id: "tgh",
+        contenuClasse: "sh-v section-content",
+        imageSrc: "images/cinema-series/films/the-great-hack-horizontal-0.jpg",
+        imageAlt: "illustration",
+        texteClasse: "sv section-text",
+        titre: "The Great Hack",
+        type: "film",
+        date: "2019-07-24",
+        description: "Le scandale de Cambridge Analytica est examiné à travers les rôles de plusieurs personnes affectées.",
+        liensClasse: "cine-links",
+        liens: [
+            { classe: "cine-link", href: "https://www.netflix.com/fr/title/80117542", target: "_blank", texte: "Voir sur Netflix" },
+            { classe: "cine-link", href: "https://www.imdb.com/title/tt9358204/", target: "_blank", texte: "Voir sur IMDB" }
+        ]
+    },
+    {
+        classe: "cine",
+        id: "tsn",
+        contenuClasse: "sh-v section-content",
+        imageSrc: "images/cinema-series/films/the-social-network-horizontal-0.jpg",
+        imageAlt: "illustration",
+        texteClasse: "sv section-text",
+        titre: "The Social Network",
+        type: "film",
+        date: "2010-10-01",
+        description: "Mark Zuckerberg, étudiant à Harvard, crée le site de réseau social qui deviendra Facebook," +
+            "mais il se retrouve poursuivi en justice par deux frères qui l'accusent d'avoir volé leur idée ainsi que par le co-fondateur de Facebook qui fut par la suite évincé de l'entreprise.",
+        liensClasse: "cine-links",
+        liens: [
+            { classe: "cine-link", href: "https://www.netflix.com/fr/title/70132721", target: "_blank", texte: "Voir sur Netflix" },
+            { classe: "cine-link", href: "https://www.imdb.com/title/tt1285016/", target: "_blank", texte: "Voir sur IMDB" }
+        ]
+    },
+    {
+        classe: "cine",
+        id: "t",
+        contenuClasse: "sh-v section-content",
+        imageSrc: "https://www.techlore.tech/assets/henry.jpeg",
+        imageAlt: "illustration",
+        texteClasse: "sv section-text",
+        titre: "Techlore",
+        type: "youtube",
+        date: "2012-02-13",
+        description: "Techlore - diffuser la confidentialité et la sécurité auprès du grand public." +
+            "Nous gérons plusieurs projets, communautés et contenus pour prouver que la confidentialité et la sécurité ne sont pas seulement réalisables, mais aussi simples et accessibles.",
+        liensClasse: "cine-links",
+        liens: [
+            { classe: "cine-link", href: "https://www.youtube.com/techlore", target: "_blank", texte: "Voir sur Youtube" },
+            { classe: "cine-link", href: "https://www.techlore.tech/", target: "_blank", texte: "Voir sur le site" }
+        ]
+    },
+    {
+        classe: "cine",
+        id: "tle",
+        contenuClasse: "sh-v section-content",
+        imageSrc: "https://c10.patreonusercontent.com/4/patreon-media/p/campaign/2341552/4b18c8eb0c5743768ebceb3d334aa2a0/eyJoIjoxMDgwLCJ3IjoxMDgwfQ%3D%3D/3.jpg?token-time=1707955200&token-hash=ijHPYyD8do15IhxjoV9K3IsV0uOo6tDial0zeGvPxew%3D",
+        imageAlt: "illustration",
+        texteClasse: "sv section-text",
+        titre: "The Linux Experiment",
+        type: "youtube",
+        date: "2018-02-21",
+        description: "Rendre Linux accessible : sans jargon technique, sans contenu ultra-technique." +
+            "Juste des actualités sur le bureau Linux, des tutoriels simples, des zooms sur des applications, et des articles d'opinion essayant de rester positifs, sans exclusion.",
+        liensClasse: "cine-links",
+        liens: [
+            { classe: "cine-link", href: "https://www.youtube.com/@TheLinuxEXP", target: "_blank", texte: "Voir sur Youtube" },
+            { classe: "cine-link", href: "https://www.patreon.com/thelinuxexperiment", target: "_blank", texte: "Voir sur Patreon" }
+        ]
+    }
+];
